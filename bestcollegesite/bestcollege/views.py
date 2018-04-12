68df23a33c4f88bb874cfe64d03d67d84f983497
Salvatore Camassa <spcamass@ncsu.edu>
2018-04-12 12:48:47 -0500

    parentheses added to print statements

--- bestcollegesite/bestcollege/views.py
+++ bestcollegesite/bestcollege/views.py
@@ -8,51 +8,55 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 
-def index( request ):
-	template = loader.get_template( 'index.html' )
-	return HttpResponse( template.render() )
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
 
-def survey( request ):
-	template = loader.get_template( 'survey.html' )
-	return HttpResponse( template.render() )
+
+def survey(request):
+    template = loader.get_template('survey.html')
+    return HttpResponse(template.render())
+
 
 @csrf_exempt
-def submit_survey( request ):
-	if request.method == 'POST':
-		#survey as json
-		data = json.loads( request.body.decode( 'utf-8' ) )
+def submit_survey(request):
+    if request.method == 'POST':
+        # survey as json
+        data = json.loads(request.body.decode('utf-8'))
 
-		#using key to find civil war response
-		print data['civilwar']
+        # using key to find civil war response
+        print(data['civilwar'])
 
-		##########################################
-		# data var is users survey answers 		 #
-		# use this var to decide on best schools #
-		# IMPORTANT: when schools are decided, 	 #
-		# make a list of their ids IN ORDER		 #
-		# 				and return	 			 #
-		# See the sample list and response below #
-		# This list is used to build the results #
-		# url so that page know what schools to  #
-		# 			  load data for 			 #
-		##########################################
+        ##########################################
+        # data var is users survey answers 		 #
+        # use this var to decide on best schools #
+        # IMPORTANT: when schools are decided, 	 #
+        # make a list of their ids IN ORDER		 #
+        # 				and return	 			 #
+        # See the sample list and response below #
+        # This list is used to build the results #
+        # url so that page know what schools to  #
+        # 			  load data for 			 #
+        ##########################################
 
-		#for dev testing
-		uni_ids = [199193, 199120, 198419, 228778, 217882, 187620, 243744, 171100, 216597, 228875]
-		return HttpResponse( json.dumps( uni_ids ) )
+        # for dev testing
+        uni_ids = [199193, 199120, 198419, 228778, 217882,
+                   187620, 243744, 171100, 216597, 228875]
+        return HttpResponse(json.dumps(uni_ids))
 
-def results( request ):
-	params = request.GET.get( 'ids', '' )
-	print params
 
-	# uni_ids is now a list of the school's ids matching
-	# the ids in the dataset
-	uni_ids = params.split( ',' )
+def results(request):
+    params = request.GET.get('ids', '')
+    print(params)
 
-	# use uni_ids to pull needed data from the dataset
-	# data will likely  be: name, city/state, enrollment/size,
-	# anything else desired in the table
-	# and latitude/longitude for the map
-	
-	template = loader.get_template( 'results.html' )
-	return HttpResponse( template.render() )
+    # uni_ids is now a list of the school's ids matching
+    # the ids in the dataset
+    uni_ids = params.split(',')
+
+    # use uni_ids to pull needed data from the dataset
+    # data will likely  be: name, city/state, enrollment/size,
+    # anything else desired in the table
+    # and latitude/longitude for the map
+
+    template = loader.get_template('results.html')
+    return HttpResponse(template.render())
